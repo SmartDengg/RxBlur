54957bbbf5b398a0c5a2f63159c7b2aa5ebdfcbd
小鄧子 <hi4joker@gmail.com>
2015-12-16 22:36:20 +0800

    format code

--- RxBlurSample/src/main/java/com/joker/blurapplication/activity/bluractivity/renderscript/RemoteBlurActivity.java
+++ RxBlurSample/src/main/java/com/joker/blurapplication/activity/bluractivity/renderscript/RemoteBlurActivity.java
@@ -46,79 +46,80 @@ public class RemoteBlurActivity extends BaseActivity implements BlurInterface {
 
   @Override public void loadBlurBitmap() {
 
-    subscription.add(remoteService.acquireBitmap()
-        .map(new Func1<Response, TransitionDrawable>() {
-          @Override public TransitionDrawable call(Response response) {
+    subscription.add(remoteService
+                         .acquireBitmap()
+                         .map(new Func1<Response, TransitionDrawable>() {
+                           @Override public TransitionDrawable call(Response response) {
 
-            RemoteBlurActivity.this.startTime = System.currentTimeMillis();
+                             RemoteBlurActivity.this.startTime = System.currentTimeMillis();
 
-            TypedInput typedInput = response.getBody();
-            Log.e(TAG, "mimeType: " + typedInput.mimeType());
+                             TypedInput typedInput = response.getBody();
+                             Log.e(TAG, "mimeType: " + typedInput.mimeType());
 
-            BufferedInputStream bufferedInputStream = null;
-            Bitmap sourceBitmap = null;
+                             BufferedInputStream bufferedInputStream = null;
+                             Bitmap sourceBitmap = null;
 
-            try {
-              bufferedInputStream = new BufferedInputStream(typedInput.in());
-              sourceBitmap = BitmapFactory.decodeStream(bufferedInputStream);
-            } catch (IOException e) {
-              Observable.error(new IOException("typedInput occur an io exception"));
-            } finally {
-              try {
-                if (bufferedInputStream != null) {
-                  bufferedInputStream.close();
-                }
-              } catch (IOException e) {
-                Observable.error(new IOException("stream close exception"));
-              }
-            }
+                             try {
+                               bufferedInputStream = new BufferedInputStream(typedInput.in());
+                               sourceBitmap = BitmapFactory.decodeStream(bufferedInputStream);
+                             } catch (IOException e) {
+                               Observable.error(new IOException("typedInput occur an io exception"));
+                             } finally {
+                               try {
+                                 if (bufferedInputStream != null) {
+                                   bufferedInputStream.close();
+                                 }
+                               } catch (IOException e) {
+                                 Observable.error(new IOException("stream close exception"));
+                               }
+                             }
 
-            Bitmap blurBitmap =
-                RxBlurEffective.bestBlur(RemoteBlurActivity.this, sourceBitmap, BLUR_RADIUS, 0.0f)
-                    .compose(RemoteBlurActivity.this.<Bitmap>bindUntilEvent(ActivityEvent.DESTROY))
-                    .toBlocking()
-                    .first();
+                             Bitmap blurBitmap = RxBlurEffective
+                                 .bestBlur(RemoteBlurActivity.this, sourceBitmap, BLUR_RADIUS, 0.0f)
+                                 .compose(RemoteBlurActivity.this.<Bitmap>bindUntilEvent(ActivityEvent.DESTROY))
+                                 .toBlocking()
+                                 .first();
 
-            Drawable[] layers = new Drawable[2];
-            layers[0] = new BitmapDrawable(RemoteBlurActivity.this.getResources(), sourceBitmap);
-            layers[1] = new BitmapDrawable(RemoteBlurActivity.this.getResources(), blurBitmap);
+                             Drawable[] layers = new Drawable[2];
+                             layers[0] = new BitmapDrawable(RemoteBlurActivity.this.getResources(), sourceBitmap);
+                             layers[1] = new BitmapDrawable(RemoteBlurActivity.this.getResources(), blurBitmap);
 
-            return new TransitionDrawable(layers);
-          }
-        })
-        .observeOn(AndroidSchedulers.mainThread())
-        .concatMap(new Func1<TransitionDrawable, Observable<Integer>>() {
-          @Override public Observable<Integer> call(TransitionDrawable transitionDrawable) {
+                             return new TransitionDrawable(layers);
+                           }
+                         })
+                         .observeOn(AndroidSchedulers.mainThread())
+                         .concatMap(new Func1<TransitionDrawable, Observable<Integer>>() {
+                           @Override public Observable<Integer> call(TransitionDrawable transitionDrawable) {
 
-            RemoteBlurActivity.this.removeLoading();
-            int count = (int) (System.currentTimeMillis() - startTime);
-            transitionDrawable.startTransition(count);
-            blurIv.setImageDrawable(transitionDrawable);
+                             RemoteBlurActivity.this.removeLoading();
+                             int count = (int) (System.currentTimeMillis() - startTime);
+                             transitionDrawable.startTransition(count);
+                             blurIv.setImageDrawable(transitionDrawable);
 
-            return Observable.range(0, count, Schedulers.computation())
-                .compose(RemoteBlurActivity.this.<Integer>bindUntilEvent(ActivityEvent.DESTROY));
-          }
-        })
-        .onBackpressureBuffer()
-        .compose(SchedulersCompat.<Integer>observeOnMainThread())
-        .subscribe(new SimpleSubscriber<Integer>() {
-          @Override public void onStart() {
-            this.request(1);
-          }
+                             return Observable
+                                 .range(0, count, Schedulers.computation())
+                                 .compose(RemoteBlurActivity.this.<Integer>bindUntilEvent(ActivityEvent.DESTROY));
+                           }
+                         })
+                         .onBackpressureBuffer()
+                         .compose(SchedulersCompat.<Integer>observeOnMainThread())
+                         .subscribe(new SimpleSubscriber<Integer>() {
+                           @Override public void onStart() {
+                             this.request(1);
+                           }
 
-          @Override public void onNext(Integer integer) {
-            durationTv.setText("" + integer + "ms");
-            this.request(1);
-          }
+                           @Override public void onNext(Integer integer) {
+                             durationTv.setText("" + integer + "ms");
+                             this.request(1);
+                           }
 
-          @Override public void onError(Throwable e) {
-            super.onError(e);
+                           @Override public void onError(Throwable e) {
+                             super.onError(e);
 
-            durationTv.setText(e.getMessage());
-          }
-        }));
+                             durationTv.setText(e.getMessage());
+                           }
+                         }));
   }
-
 
   @Override public int getLayoutId() {
     return R.layout.activity_simple_layout;
@@ -128,15 +129,13 @@ public class RemoteBlurActivity extends BaseActivity implements BlurInterface {
     getSupportActionBar().setTitle("Remote Blur");
     durationTv.setText("retrieving image from remote,wait......");
 
-    RestAdapter restAdapter =
-        new RestAdapter.Builder().setEndpoint(BASE_URL).setClient(new OkClient()).build();
+    RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(BASE_URL).setClient(new OkClient()).build();
 
     remoteService = restAdapter.create(RestApi.class);
   }
 
   interface RestApi {
 
-    @GET("/images/2015/12/06/4aa43364c2144aa9a7521cc7b33acc08.png")
-    Observable<Response> acquireBitmap();
+    @GET("/images/2015/12/06/4aa43364c2144aa9a7521cc7b33acc08.png") Observable<Response> acquireBitmap();
   }
 }
